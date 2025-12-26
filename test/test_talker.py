ab24c41738d5c04a52930f49c0519adc59341e7e
Itto Hase <abannofabiao@gmail.com>
2025-12-27 02:15:09 +0900

    testリベンジ9

--- test/test_talker.py
+++ test/test_talker.py
@@ -1,3 +1,33 @@
-def test_dummy():
-    assert True
+import pytest
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+import subprocess
+import time
+import signal
+
+@pytest.fixture(scope="module")
+def talker():
+    # talker.py を裏で起動
+    proc = subprocess.Popen(["python3", "mypkg/talker.py"])
+    time.sleep(2)  # 起動待ち
+    yield
+    proc.send_signal(signal.SIGINT)
+    proc.wait()
+
+def test_query_service(talker):
+    rclpy.init()
+    node = Node("test_node")
+    client = node.create_client(Query, "query")
+    while not client.wait_for_service(timeout_sec=1.0):
+        pass
+
+    req = Query.Request()
+    req.name = "now"
+    future = client.call_async(req)
+    rclpy.spin_until_future_complete(node, future)
+    result = future.result()
+    assert result.age == 123
+    node.destroy_node()
+    rclpy.shutdown()
 
