a92230e3f8fa3bbdd0dcb75a26d4f7ad4a3054fb
Itto Hase <abannofabiao@gmail.com>
2026-01-08 20:25:59 +0900

    テスト授業版5

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,16 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
+# サービスサーバ起動
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log &
+
+# 起動待ち（超重要・最低限）
+sleep 2
+
+# サービス呼び出し（これが通信テスト）
+ros2 service call /query person_msgs/srv/Query "time: now"
+
+# ログ確認（正しい grep）
 grep "now:" /tmp/mypkg.log
+
