33feb9189523eb8ff769e90d55d00f5ae7c8c06f
Itto Hase <abannofabiao@gmail.com>
2026-01-08 20:31:30 +0900

    テスト授業版6

--- test/test.bash
+++ test/test.bash
@@ -13,6 +13,10 @@ timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log &
 # 起動待ち（超重要・最低限）
 sleep 2
 
+# ③ 同じ端末・同じ環境で確認
+ros2 service list
+ros2 service type /query
+
 # サービス呼び出し（これが通信テスト）
 ros2 service call /query person_msgs/srv/Query "time: now"
 
